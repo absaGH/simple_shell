c2f3ec963b002229d6cd49ad9cf7ba9752857197
absaGH <absa3852@gmail.com>
2021-04-11 14:23:14 +0000

    simple shell commit

--- builtin.c
+++ builtin.c
@@ -1,50 +1,32 @@
 #include "shell.h"
 
-char *builtin_str[] = {
-	"help", "exit"
-};
-
-int (*builtin_func[]) (char **) = {
-	&sh_help, &sh_exit
-};
-
 /**
- * sh_num_builtins - findout the number of builtin commands
- *
- * Return: number of builtins
+ * builtin_fun - Execute builtin_fun.
+ * @args: args Null terminated list of arguments.
+   
+ * Return nothing
  */
-int sh_num_builtins(void)
-{
-	return (sizeof(builtin_str) / sizeof(char *));
-}
 
-/**
- *  sh_help - print help.
- *  @args: args List of args.
- *  Return: Always returns 1, to continue executing.
- */
-int sh_help(char **args)
+void builtin_fun(char **args)
 {
-	_puts("Use the man command for information on other programs.\n");
-	return (1);
-}
-
-/**
- *sh_exit - exit the line
- *Return: always zero
- */
-int sh_exit(char **args)
-{
-	exit(0);
+  builtins_t b[] = {
+    {NULL, NULL}
+  };
+  int i;
+  for (i = 0; b[i].f != NULL; i++)
+    {
+      if(_strcmp(args[0], b[i].name) == 0)
+	b[i].f(args);
+    }
 }
 
 /**
  * sh_execute - Execute shell built-in or launch program.
  * @args: args Null terminated list of arguments.
-
+ *
  * Return 1 if the shell should continue running, 0 if it should terminate
  */
-int sh_execute(char **args)
+int sh_execute(char **args, char *av, char **env, unsigned int cont)
 {
 	int i;
 
@@ -53,13 +35,7 @@ int sh_execute(char **args)
 		return (1);
 	}
 
-	for (i = 0; i < sh_num_builtins(); i++)
-	{
-		if (_strcmp(args[0], builtin_str[i]) == 0)
-		{
-			return ((*builtin_func[i])(args));
-		}
-	}
+	builtin_fun(args);	
 
-	return (sh_launch(args));
+	return (sh_launch(args, av, env, cont));
 }
