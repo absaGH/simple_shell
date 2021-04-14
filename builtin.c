a63ab3540ae9bc22044a439951028ae7ac4e8e4b
absaGH <absa3852@gmail.com>
2021-04-14 13:36:18 +0000

    simple shell

--- builtin.c
+++ builtin.c
@@ -7,16 +7,47 @@
  * Return nothing
  */
 
-void builtin_fun(char **args)
+void builtin_fun(char **args, char **env)
 {
   builtins_t b[] = {
+    {"exit", sh_exit},
+    {"env", sh_env},
     {NULL, NULL}
   };
   int i;
   for (i = 0; b[i].f != NULL; i++)
     {
       if(_strcmp(args[0], b[i].name) == 0)
-	b[i].f(args);
+	b[i].f(args, env);
+    }
+}
+
+/**
+ * sh_exit - Builtin command: exit.
+ * @args: List of args.
+ * 
+ *Return: Always returns 0, to terminate execution.
+ */
+void sh_exit(char **args, char **env __attribute__((unused)))
+{
+  dobfreer(args);
+  exit(0);
+}
+
+/**
+ * sh_env - prints the current environment
+ * @args: list of arguments
+ * @env: variable that contain the current environment
+ * Return: void.
+ */
+void sh_env(char **args __attribute__((unused)), char **env)
+{
+  unsigned int i;
+
+  for (i = 0; env[i]; i++)
+    {
+      _puts(env[i]);
+      _puts("\n");
     }
 }
 
